693c6774d1c742fa466779b41893df891bfa52fe
guuulsss <91806511+guuulsss@users.noreply.github.com>
2024-10-28 01:50:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from simulate import simulate_system, plot_response
+import sys
+from PyQt5 import QtWidgets
+from simulation import Simulation
+from plotter import Plotter
+from gui import GuiApp
 
-if __name__ == "__main__":
-    t, y = simulate_system()
-    plot_response(t, y)
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    simulation = Simulation(initial_state=[10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            d_func=lambda t: 0.1,
+                            Fthrust_func=lambda t: 0,
+                            H_func=lambda z: 100)
+    plotter = Plotter(canvas=None, param_canvas=None)  # Инициализация с реальными объектами
+    window = GuiApp(simulation, plotter)
+    window.show()
+    sys.exit(app.exec_())
